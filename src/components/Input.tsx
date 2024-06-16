a0fb60daf3fdf5766a5b9b98e64807b2c65fc8f4
kmr-ankitt <sahuankit.murhu@gmail.com>
2024-06-16 16:04:11 +0530

    sending input data

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,16 +1,19 @@
-import { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
 
 type FormValues = {
   playlist: string;
 };
 
-function Input() {
-  const [playlistID, setPlaylistID] = useState<string>("");
+type InputProps = {
+  sendID: (id: string) => void;
+};
+
+function Input({ sendID }: InputProps) {
   const { register, handleSubmit } = useForm<FormValues>();
 
   const extractPlaylistID = (url: string): string => {
     try {
+      // Ensuring the URL has a protocol
       if (!url.startsWith('http://') && !url.startsWith('https://')) {
         url = 'https://' + url;
       }
@@ -24,10 +27,8 @@ function Input() {
 
   const onSubmit: SubmitHandler<FormValues> = data => {
     const id = extractPlaylistID(data.playlist);
-    setPlaylistID(id);
+    sendID(id);  
   };
-  console.log(playlistID);
-  
 
   return (
     <div>
@@ -35,7 +36,7 @@ function Input() {
         <input
           type="text"
           placeholder="Playlist URL"
-          className="p-2 w-1/3 min-w-[16rem] placeholder:text-center bg-zinc-200 text-zinc-600 outline-none" required autoComplete='off'
+          className="p-2 w-1/3 min-w-[16rem] placeholder:text-center bg-zinc-200 text-zinc-600 outline-none"
           {...register('playlist', { required: true })}
         />
         <input type="submit" value="submit" className="p-2 bg-[#DF470A] uppercase w-1/3 min-w-[16rem]" />
