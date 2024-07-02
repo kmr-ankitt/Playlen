1403ca44502584b8e1a453a01e2131fca0ebc594
Ankit kumar sahu <sahuankit.murhu@gmail.com>
2024-07-02 18:55:09 +0530

    average duration of video backend

--- backend/api.js
+++ backend/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 import "dotenv/config";
 import bodyParser from "body-parser";
 import cors from "cors";
-import { parseISODurationToMinutes } from "./decoder";
+import { parseISODurationToMinutes } from "./decoder.js";
 
 const app = express();
 const port = 5000;
@@ -11,7 +11,6 @@ const port = 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-
 
 async function getVideoDuration(videoIds) {
   try {
@@ -29,11 +28,40 @@ async function getVideoDuration(videoIds) {
       const data = response.data.items[0].contentDetails.duration;
       duration += parseISODurationToMinutes(data);
     }
-
-    console.log(duration)
-    return duration;
+    return { totalDuration: duration, videoCount: videoIds.length };
   } catch (error) {
     console.log("Error fetching video duration:", error);
+    return null;
+  }
+}
+
+async function fetchAllVideos(playlistId) {
+  let videoIDs = [];
+  let nextPageToken = "";
+  try {
+    do {
+      const response = await axios.get(
+        "https://www.googleapis.com/youtube/v3/playlistItems",
+        {
+          params: {
+            part: "snippet",
+            playlistId: playlistId,
+            maxResults: 50,
+            pageToken: nextPageToken,
+            // eslint-disable-next-line no-undef
+            key: process.env.YOUTUBE_API_KEY,
+          },
+        }
+      );
+
+      const data = response.data;
+      nextPageToken = data.nextPageToken || "";
+      videoIDs = videoIDs.concat(data.items.map((item) => item.snippet.resourceId.videoId));
+    } while (nextPageToken);
+
+    return videoIDs;
+  } catch (error) {
+    console.error("Error fetching playlist items:", error);
     return null;
   }
 }
@@ -41,29 +69,17 @@ async function getVideoDuration(videoIds) {
 app.post("/api/playlistItems", async (req, res) => {
   console.log("Request received");
   try {
-    const response = await axios.get(
-      "https://www.googleapis.com/youtube/v3/playlistItems",
-      {
-        params: {
-          part: "snippet",
-          playlistId: req.body.pID,
-          maxResults: 50,
-          // eslint-disable-next-line no-undef
-          key: process.env.YOUTUBE_API_KEY,
-        },
-      }
-    );
-
-    const data = response.data;
-    const videoIDs = data.items.map((item) => item.snippet.resourceId.videoId);
-    console.log(videoIDs);
-
-    const duration = await getVideoDuration(videoIDs);
-    res.status(200).send(`${duration}`);
-
+    const videoIDs = await fetchAllVideos(req.body.pID);
+    if (videoIDs) {
+      const { totalDuration, videoCount } = await getVideoDuration(videoIDs);
+      const averageDuration = Math.round(totalDuration / videoCount);
+      res.status(200).json({ totalDuration, averageDuration});
+    } else {
+      res.status(500).send("Error fetching playlist items");
+    }
   } catch (error) {
     console.error("Error fetching playlist items:", error);
-    res.status(500).send("Error fetching playlist items", error);
+    res.status(500).send("Error fetching playlist items");
   }
 });
 
